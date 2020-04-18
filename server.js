c80cbb9dae6c0f14817151db40fe1e051cdaa96d
Sanjay Rathdo <kevin.macwan@nstacksoftech.com>
2020-04-18 16:30:23 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+//Load env vars
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
